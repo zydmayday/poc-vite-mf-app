ada9ffe1bb86bc47c55939cc34b2ed38ab581038
zyd <zydmayday@gmail.com>
2023-11-02 01:00:25 +0900

    react use federation-plugin

---
+++ host/src/vite-env.d.ts
@@ -0,0 +1,6 @@
+/// <reference types="vite/client" />
+declare module "remoteApp/Button" {
+  import { FC } from "react";
+  const Button: FC<>;
+  export default Button;
+}
---
+++ remote/src/components/Button.tsx
@@ -0,0 +1,22 @@
+import "./Button.css";
+
+import { useState } from "react";
+
+export const Button = () => {
+  const [state, setState] = useState(0);
+  return (
+    <>
+      <div>
+        <button
+          id="click-btn"
+          className="shared-btn"
+          onClick={() => setState((s) => s + 1)}
+        >
+          Click me here: {state}
+        </button>
+      </div>
+    </>
+  );
+};
+
+export default Button;
